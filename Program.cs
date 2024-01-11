adb69d271da9f7b6dbef7ac9474a340e0dbfeac8
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-01-11 13:48:00 -0300

    finishing challenge of POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("11980613999", "iphone", "12391255", 1000);
+Nokia nokia = new Nokia("11980613998", "XY", "12391255", 2000);
+
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DioApp");
+
+
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("NokiaDioApp");
